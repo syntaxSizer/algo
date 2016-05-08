b58d2d39a897a620a2fbd40e4130a9ff55c0f0fd
syntaxSizer <kakuma4never@gmail.com>
2016-05-08 09:59:33 +0300

    add  Insertion sort algorithums

---
+++ insertion-Sort.java
@@ -0,0 +1,43 @@
+
+/**
+ *
+ * @author Qusai Alfaki
+ */
+public class MyInsertionSort {
+
+    /**
+     *--------Insertion Sort---------
+     *algorithm
+     */
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int arr1 []= {3,6,3,8,7,0};
+        int[] arr2=insertsort(arr1);
+// loop just to print the sorted array
+        for (int i:arr2){
+            System.out.print(i);
+            System.out.print(", ");
+        }
+
+   }
+ public static int[] insertsort(int [] input){
+// here where the magic happens
+// the first for loop will loop through all the array while the second loop will loop up to i+1
+// meaning
+     int temp;
+     for (int i =1; i<input.length; i++){
+         for (int j=i; j>0; j--)
+         {
+           if(input[j]<input[j-1]){
+             temp=input[j];
+             input[j]=input[j-1];
+             input[j-1]=temp;
+         }
+         }
+     }
+     return input;
+         }
+ }
+
+
